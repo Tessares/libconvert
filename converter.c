ca86224d13a5d2cf01e9ca85016d34113bc5f44f
Gregory Detal <gregory.detal@tessares.net>
2019-01-17 10:26:18 +0100

    add log.c as logging facility
    environment CONVERTER_LOG=<file> is used as log.
    
    Change-Id: I684473de39964ba156f7a78ce9bfed4554ccaf53
    Signed-off-by: Gregory Detal <gregory.detal@tessares.net>

--- converter.c
+++ converter.c
@@ -30,7 +30,9 @@
  */
 
 #include <libsyscall_intercept_hook_point.h>
+#include <log.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <pthread.h>
@@ -61,6 +63,9 @@ typedef struct socket_state {
 static LIST_HEAD(, socket_state) _socket_htable[NUM_BUCKETS];
 /* note: global hash table mutex, improvement: lock per bucket */
 static pthread_mutex_t _socket_htable_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static FILE *		_log;
+static pthread_mutex_t	_log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static int
 _hash(int fd)
@@ -96,6 +101,8 @@ _alloc(int fd)
 	if (!state)
 		return;
 
+	log_debug("allocate state for fd %d", fd);
+
 	state->fd = fd;
 
 	pthread_mutex_lock(&_socket_htable_mutex);
@@ -113,6 +120,8 @@ _free(int fd)
 		/* no state associated with this fd. */
 		return;
 
+	log_debug("release state for fd %d", fd);
+
 	pthread_mutex_lock(&_socket_htable_mutex);
 	LIST_REMOVE(state, list);
 	pthread_mutex_unlock(&_socket_htable_mutex);
@@ -128,8 +137,12 @@ _handle_socket(long arg0, long arg1, long arg2, long *result)
 	if (((arg0 == AF_INET) || (arg0 == AF_INET6)) && (arg1 == SOCK_STREAM)) {
 		int fd;
 
+		log_debug("handle socket(%ld, %ld, %ld)", arg0, arg1, arg2);
+
 		/* execute the socket() syscall to learn the file descriptor */
 		*result = syscall_no_intercept(SYS_socket, arg0, arg1, arg2);
+
+		log_debug("-> fd: %d", (int)*result);
 
 		if (*result >= 0)
 			_alloc((int)*result);
@@ -173,32 +186,43 @@ _read_sysctl_fastopen(unsigned int *flags)
 	char *	endp;
 
 	fd = open("/proc/sys/net/ipv4/tcp_fastopen", O_RDONLY);
-	if (fd < 0)
+	if (fd < 0) {
+		log_error("unable to open /proc/sys/net/ipv4/tcp_fastopen: %s",
+		          strerror(errno));
 		return fd;
+	}
 
 	rc = read(fd, buffer, sizeof(buffer));
 	close(fd);
 
-	if (rc < 0)
+	if (rc < 0) {
+		log_error("unable to read /proc/sys/net/ipv4/tcp_fastopen: %s",
+		          strerror(errno));
 		return rc;
+	}
 
 	/* contains a base 10 number */
 	*flags = strtol(buffer, &endp, 10);
 
-	if (*endp && *endp != '\n')
+	if (*endp && *endp != '\n') {
+		log_error("unable to parse /proc/sys/net/ipv4/tcp_fastopen");
 		return -1;
+	}
 
 	return 0;
 }
 
 static int
-_check_sysctl_fastopen(void)
+_validate_sysctl_fastopen(void)
 {
 	unsigned int	flags		= 0;
 	unsigned int	expected_flags	= (0x1 | 0x4);
 
 	if (_read_sysctl_fastopen(&flags) < 0)
 		return -1;
+
+	log_debug("fastopen sysctl is %x, expect flags %x to be set", flags,
+	          expected_flags);
 
 	/* We use the fastopen backend without requiring exchange of actual
 	 * fastopen options. expected_flags are: 0x1 (sending data in SYN) and
@@ -210,13 +234,46 @@ _check_sysctl_fastopen(void)
 	return 0;
 }
 
+static void
+_log_lock(void *udata, int lock)
+{
+	if (lock)
+		pthread_mutex_lock(&_log_mutex);
+	else
+		pthread_mutex_unlock(&_log_mutex);
+}
+
 static __attribute__((constructor)) void
 init(void)
 {
-	if (_check_sysctl_fastopen() < 0)
-		fprintf(stderr, "Disabling the interception of TCP connections, setup "
-		        "the sysctl tcp_fastopen to an appropriate value\n");
+	const char *log_path = getenv("CONVERTER_LOG");
+
+	log_set_quiet(1);
+	log_set_lock(_log_lock);
+
+	/* open the log iff specified */
+	if (log_path) {
+		_log = fopen(log_path, "w");
+		if (!_log)
+			fprintf(stderr, "converter: unable to open log %s: %s",
+			        log_path, strerror(errno));
+		log_set_fp(_log);
+	}
+
+	log_info("Starting interception");
+
+	if (_validate_sysctl_fastopen() < 0)
+		log_error("Disabling the interception of TCP connections, setup "
+		          "the sysctl tcp_fastopen to an appropriate value^");
 	else
 		/* Set up the callback function */
 		intercept_hook_point = _hook;
+}
+
+static __attribute__((destructor)) void
+fini(void)
+{
+	log_info("Terminating interception");
+	if (_log)
+		fclose(_log);
 }
