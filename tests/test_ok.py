2f97d856cec91f5f9c2f5161050f5cdf226eed4f
Gregory Vander Schueren <gregory.vanderschueren@tessares.net>
2020-09-14 16:08:17 +0200

    tests: add IPv6 converter tests
    Currently failing as the Docker has not IPv6 enabled.
    (getaddrinfo returns: "unable to resolve '::1/128'").
    
    Apprently we can't enable IPv6 in the Docker without changing the
    config of the Docker daemon. So I will probably abandon this patch.
    
    Signed-off-by: Gregory Vander Schueren <gregory.vanderschueren@tessares.net>

--- tests/test_ok.py
+++ tests/test_ok.py
@@ -5,11 +5,11 @@ from test_lib import *
 
 
 class TestOK(TestInstance):
-    def run(self):
+    def server(self):
         class MockConverterOK(MockConverter):
             actions = [RecvSyn(), SendSynAck(payload=Convert().build()), RecvHTTPGet(
             ), SendHTTPResp("HELLO, WORLD!"), SendPkt(flags='RA')]
-        MockConverterOK()
+        MockConverterOK(address=self.converter_adress())
 
     def validate(self):
         self.assert_result("HELLO, WORLD!")
